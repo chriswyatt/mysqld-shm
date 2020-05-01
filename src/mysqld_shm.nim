eb80cab586b2a5bbbab492fd252b018f0595c950
Chris Wyatt <chris.wyatt@sgdigital.com>
2020-05-01 10:06:55 +0100

    Initial commit

---
+++ src/mysqld_shm.nim
@@ -0,0 +1,75 @@
+import os
+import osproc
+import strutils
+import threadpool
+
+const mysqlVarPath = "/dev/shm/mysql_var"
+
+const chownCmd = "chown mysql:mysql " & mysqlVarPath
+
+const runuserParams = @["-u", "mysql", "-g", "mysql", "mysqld"]
+
+const initCmd = join(
+    [
+        "mariadb-install-db",
+        "--user=mysql",
+        "--basedir=/usr",
+        "--datadir=" & mysqlVarPath,
+    ],
+    " ",
+)
+
+const sql = join(
+    [
+        "CREATE USER 'portal_app'@'localhost' IDENTIFIED BY 'portal_app'",
+        "GRANT ALL PRIVILEGES ON *.* TO 'portal_app'@'localhost'",
+        "FLUSH PRIVILEGES",
+    ],
+    "; ",
+)
+
+if existsOrCreateDir(mysql_var_path):
+    quit(QuitSuccess)
+
+block:
+    let exitCode = execCmd(chownCmd)
+    if exitCode != 0:
+        quit(exit_code)
+
+block:
+    let exitCode = execCmd(initCmd)
+    if exitCode != 0:
+        quit(exit_code)
+
+let mysqldFlowVar = spawn execProcess(
+    "runuser",
+    args = runuserParams & commandLineParams(),
+    options = {poEchoCmd, poStdErrToStdOut, poUsePath},
+)
+
+block:
+    var ready = false
+
+    for i in 0..9:
+        sleep(1000)
+        ready = execCmd("mysqladmin ping") == 0
+
+        if ready:
+            break
+
+block:
+    let output = execProcess(
+        "mysql",
+        args = ["-u", "root", "--execute=" & sql],
+        options = {poEchoCmd, poStdErrToStdOut, poUsePath},
+    )
+    echo output
+
+block:
+    let exitCode = execCmd("mysqladmin shutdown")
+    if exitCode != 0:
+        quit(QuitFailure)
+
+sync()
+let mysqldOutput = ^mysqldFlowVar
+echo mysqldOutput
